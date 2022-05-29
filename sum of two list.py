6ff266e5201cec043c792a027f18aefff4741158
rudranshpandey09 <106270430+rudranshpandey09@users.noreply.github.com>
2022-05-29 12:58:41 +0530

    python program to print sum of two list

---
+++ sum of two list.py
@@ -0,0 +1,18 @@
+l1=[]
+l2=[]
+
+
+a=int(input("enter the no. elements:"))
+for i in range(0, a):
+    x = int(input())
+    l1.append(x)
+
+
+    
+b=int(input("enter the no. of elements:"))
+for i in range(0,b):
+    y=int(input())
+    l2.append(y)
+
+    
+print("addition of two list:",l1+l2)
